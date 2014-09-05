59f811cf0d5dc09dd9999073fb981046dc0e89cb
CROFT James <jacr7386@jiliac.colorado.edu>
2014-09-05 14:07:43 -0600

    fig1 nearly finished only minor tweeking needed

--- fig1.py
+++ fig1.py
@@ -1,6 +1,8 @@
 import DOSModule
 import numpy as np
 import pylab as plt
+from jfec_colour import jfec_colour_scheme
+
 
 filen ='Jfec_AlkaliAtomPlusDimer.txt'
 Jfec_AlkaliAtomPlusDimer = DOSModule.AtomMoleculeDOS(filen)
@@ -10,25 +12,30 @@ filen ='HeliumPlusHydrocarbon.txt'
 HeliumPlusHydrocarbon = DOSModule.AtomMoleculeDOS(filen)
 #choose which set of systems to use
 systems = HeliumPlusHydrocarbon
+#set up color scheme
+colors = jfec_colour_scheme(len(systems.systems.keys()),0,0.8)
 
 
 J,MQN = 0,0 #total J quantum number
 nmax,vmax = 100, 9999#maximum rotational and vibrational quantum numbers
 #lmax is determined by J and nmax
 collision_energies = np.logspace(-2,3,11)
-collision_energy = 1.0e-9
+#collision_energy = 1.0e-9
 
 
 for system in systems.systems.keys():
  print system
+ color = colors.next()
  NumOpen_Iter = (systems.get_num_open(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy) for collision_energy in collision_energies)
- Lifetime_Iter = (systems.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy)[1] for collision_energy in collision_energies)
- Lifetime_ZeroEnergy_Iter = (systems.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax)[1] for collision_energy in collision_energies)
+ NumOpen_J3_Iter = (systems.get_num_open(system,J=3,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy) for collision_energy in collision_energies)
+ Lifetime_Iter = (systems.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax)[1] for collision_energy in collision_energies)
+ Lifetime_J3_Iter = (systems.get_dos(system,J=3,MQN=MQN,nmax=nmax,vmax=vmax)[1] for collision_energy in collision_energies)
  NumOpen  = np.fromiter(NumOpen_Iter,np.int_)
+ NumOpen_J3  = np.fromiter(NumOpen_J3_Iter,np.int_)
  Lifetime = np.fromiter(Lifetime_Iter,np.float)
- Lifetime_ZeroEnergy = np.fromiter(Lifetime_ZeroEnergy_Iter,np.float)
- plt.plot(collision_energies,Lifetime/NumOpen,label='Helium+'+system.split('_')[1].capitalize())
- plt.plot(collision_energies,Lifetime_ZeroEnergy/NumOpen,'--')
+ Lifetime_J3 = np.fromiter(Lifetime_J3_Iter,np.float)
+ plt.plot(collision_energies,Lifetime/NumOpen,label='Helium+'+system.split('_')[1].capitalize(),color=color)
+ plt.plot(collision_energies,Lifetime_J3/NumOpen_J3,'--',color=color)
 
 plt.rcParams.update({'font.size': 22})
 plt.legend(loc=0,shadow=True)
