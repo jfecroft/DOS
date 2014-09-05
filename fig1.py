3e1aff9cf23dfb8d7c3c5d8d7da4ce816bf7f60c
CROFT James <jacr7386@jiliac.colorado.edu>
2014-09-05 13:26:39 -0600

    added python script to make figure1 for publication needs finishing

--- fig1.py
+++ fig1.py
@@ -1,5 +1,6 @@
 import DOSModule
 import numpy as np
+import pylab as plt
 
 filen ='Jfec_AlkaliAtomPlusDimer.txt'
 Jfec_AlkaliAtomPlusDimer = DOSModule.AtomMoleculeDOS(filen)
@@ -7,23 +8,32 @@ filen ='Mayle_AlkaliAtomPlusDimer.txt'
 Mayle_AlkaliAtomPlusDimer = DOSModule.AtomMoleculeDOS(filen)
 filen ='HeliumPlusHydrocarbon.txt'
 HeliumPlusHydrocarbon = DOSModule.AtomMoleculeDOS(filen)
+#choose which set of systems to use
+systems = HeliumPlusHydrocarbon
 
 
 J,MQN = 0,0 #total J quantum number
 nmax,vmax = 100, 9999#maximum rotational and vibrational quantum numbers
 #lmax is determined by J and nmax
-collision_energies = np.logspace(-2,1,11)
+collision_energies = np.logspace(-2,3,11)
 collision_energy = 1.0e-9
 
-print 'system  dos(mK-1) lt(ns)'
-#printing dos and lt for alkali atom + dimer collisions as published in
-#Long-Lived Complexes and Chaos in Ultracold Molecular Collisions
-#            J. F. E. Croft and J. L. Bohn, Phys. Rev. A 89, 102714 (2014). 
-for system in Jfec_AlkaliAtomPlusDimer.systems.keys():
- Jfec_AlkaliAtomPlusDimer.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy)
- NumOpen_Iter = (Jfec_AlkaliAtomPlusDimer.get_num_open(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy) for collision_energy in collision_energies)
- for i in range(8):
-  print NumOpen_Iter.next()
-# NumOpen = np.fromiter(NumOpen_Iter,np.int_)
-# print NumOpen
-# print system,Jfec_AlkaliAtomPlusDimer.dos,Jfec_AlkaliAtomPlusDimer.lt, Jfec_AlkaliAtomPlusDimer.num_open
+
+for system in systems.systems.keys():
+ print system
+ NumOpen_Iter = (systems.get_num_open(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy) for collision_energy in collision_energies)
+ Lifetime_Iter = (systems.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy)[1] for collision_energy in collision_energies)
+ Lifetime_ZeroEnergy_Iter = (systems.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax)[1] for collision_energy in collision_energies)
+ NumOpen  = np.fromiter(NumOpen_Iter,np.int_)
+ Lifetime = np.fromiter(Lifetime_Iter,np.float)
+ Lifetime_ZeroEnergy = np.fromiter(Lifetime_ZeroEnergy_Iter,np.float)
+ plt.plot(collision_energies,Lifetime/NumOpen,label='Helium+'+system.split('_')[1].capitalize())
+ plt.plot(collision_energies,Lifetime_ZeroEnergy/NumOpen,'--')
+
+plt.rcParams.update({'font.size': 22})
+plt.legend(loc=0,shadow=True)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Collision Energy (K)')
+plt.ylabel('Lifetime (ns)')
+plt.show()
