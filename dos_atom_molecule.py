1ad1f7871c04d4945f49fc8662c26dfad3a960cf
jfecroft <j.f.e.croft@gmail.com>
2015-03-25 19:30:53 +0000

    [cl] pyint

--- dos_atom_molecule.py
+++ dos_atom_molecule.py
@@ -1,36 +1,54 @@
+"""
+Reproduce published results
+"""
 import DOSModule
 
-filen ='Jfec_AlkaliAtomPlusDimer.txt'
-Jfec_AlkaliAtomPlusDimer = DOSModule.AtomMoleculeDOS(filen)
-filen ='Mayle_AlkaliAtomPlusDimer.txt'
-Mayle_AlkaliAtomPlusDimer = DOSModule.AtomMoleculeDOS(filen)
-filen ='HeliumPlusHydrocarbon.txt'
-HeliumPlusHydrocarbon = DOSModule.AtomMoleculeDOS(filen)
+FILEN = 'Jfec_AlkaliAtomPlusDimer.txt'
+JFEC_ALKALIATOMPLUSDIMER = DOSModule.AtomMoleculeDOS(FILEN)
+FILEN = 'Mayle_AlkaliAtomPlusDimer.txt'
+MAYLE_ALKALIATOMPLUSDIMER = DOSModule.AtomMoleculeDOS(FILEN)
+FILEN = 'HeliumPlusHydrocarbon.txt'
+HELIUMPLUSHYDROCARBON = DOSModule.AtomMoleculeDOS(FILEN)
 
 
-J,MQN = 0,0 #total J quantum number
-nmax,vmax = 100, 9999#maximum rotational and vibrational quantum numbers
-#lmax is determined by J and nmax
-collision_energy = 0.0
+J, MQN = 0, 0  # total J quantum number
+NMAX, VMAX = 100, 9999  # maximum rotational and vibrational quantum numbers
+# lmax is determined by J and NMAX
+COLLISION_ENERGY = 0.0
 
 print 'system  dos(mK-1) lt(ns)'
-#printing dos and lt for alkali atom + dimer collisions as published in
-#Long-Lived Complexes and Chaos in Ultracold Molecular Collisions
-#            J. F. E. Croft and J. L. Bohn, Phys. Rev. A 89, 102714 (2014). 
-for system in Jfec_AlkaliAtomPlusDimer.systems.keys():
- dos, lt =Jfec_AlkaliAtomPlusDimer.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy)
- print system,dos,lt
+# printing dos and lt for alkali atom + dimer collisions as published in
+# Long-Lived Complexes and Chaos in Ultracold Molecular Collisions
+#            J. F. E. Croft and J. L. Bohn, Phys. Rev. A 89, 102714 (2014).
+for system in JFEC_ALKALIATOMPLUSDIMER.systems.keys():
+    dos, lt = JFEC_ALKALIATOMPLUSDIMER.get_dos(system,
+                                               J=J,
+                                               MQN=MQN,
+                                               nmax=NMAX,
+                                               vmax=VMAX,
+                                               energy_offset=COLLISION_ENERGY)
+    print system, dos, lt
 
 
-#printing dos and lt for alkali atom + dimer collisions as published in
-#statistical Aspects of Ultracold Resonant Scattering-- M. Mayle, B. P. Ruzic, and J. L. Bohn, Phys. Rev. A 85, 062712 (2012).
-for system in Mayle_AlkaliAtomPlusDimer.systems.keys():
- dos, lt = Mayle_AlkaliAtomPlusDimer.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy)
- print system,dos,lt
+# printing dos and lt for alkali atom + dimer collisions as published in
+# statistical Aspects of Ultracold Resonant Scattering --
+# M. Mayle, B. P. Ruzic, and J. L. Bohn, Phys. Rev. A 85, 062712 (2012).
+for system in MAYLE_ALKALIATOMPLUSDIMER.systems.keys():
+    dos, lt = MAYLE_ALKALIATOMPLUSDIMER.get_dos(system,
+                                                J=J,
+                                                MQN=MQN,
+                                                nmax=NMAX,
+                                                vmax=VMAX,
+                                                energy_offset=COLLISION_ENERGY)
+    print system, dos, lt
 
-#printing dos and lt for helium + hydrocarbon collisions 
-#as yet unpublished
-for system in HeliumPlusHydrocarbon.systems.keys():
- dos, lt = HeliumPlusHydrocarbon.get_dos(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax,energy_offset=collision_energy)
- print system,dos,lt
-
+# printing dos and lt for helium + hydrocarbon collisions
+# as yet unpublished
+for system in HELIUMPLUSHYDROCARBON.systems.keys():
+    dos, lt = HELIUMPLUSHYDROCARBON.get_dos(system,
+                                            J=J,
+                                            MQN=MQN,
+                                            nmax=NMAX,
+                                            vmax=VMAX,
+                                            energy_offset=COLLISION_ENERGY)
+    print system, dos, lt
