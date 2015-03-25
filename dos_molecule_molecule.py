5797bfa9eeb7c8fce6988ecc5c3f3c6a64ae3e1e
jfecroft <j.f.e.croft@gmail.com>
2015-03-25 19:39:13 +0000

    [cl] pylint

--- dos_molecule_molecule.py
+++ dos_molecule_molecule.py
@@ -1,25 +1,36 @@
+"""
+Reproduce publish molecule moleucle lifetimes
+"""
 import DOSModule
-#these can take awhile (days) to run for full converged results
 
-filen ='Jfec_AlkaliDimerPlusDimer.txt'
-Jfec_AlkaliDimerPlusDimer = DOSModule.MoleculeMoleculeDOS(filen)
-filen ='Mayle_AlkaliDimerPlusDimer.txt'
-Mayle_AlkaliDimerPlusDimer = DOSModule.MoleculeMoleculeDOS(filen)
-J,MQN = 0,0 #total J quantum number
-nmax,vmax = 5, 5#maximum rotational and vibrational quantum numbers
-#lmax is determined by J and nmax
+# these can take awhile (days) to run for full converged results
 
-
+FILEN = 'Jfec_AlkaliDimerPlusDimer.txt'
+JFEC_ALKALIDIMERPLUSDIMER = DOSModule.MoleculeMoleculeDOS(FILEN)
+FILEN = 'Mayle_AlkaliDimerPlusDimer.txt'
+MAYLE_ALKALIDIMERPLUSDIMER = DOSModule.MoleculeMoleculeDOS(FILEN)
+J, MQN = 0, 0  # total J quantum number
+NMAX, VMAX = 5, 5  # maximum rotational and vibrational quantum numbers
+# lmax is determined by J and NMAX
 
 print 'system  dos(uK-1) lt(ms)'
 
-#reproducing data published in 
-#Scattering of Ultracold Molecules in the Highly Resonant Regime -- M. Mayle, G. Quemener, B. P. Ruzic, and J. L. Bohn, Phys. Rev. A 87, 012709 (2013).
-for system in Mayle_AlkaliDimerPlusDimer.systems.keys():
- dos, lt = Mayle_AlkaliDimerPlusDimer.get_dos_MoleculeMolecule(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax)
- print system, dos, lt
+# reproducing data published in
+# Scattering of Ultracold Molecules in the Highly Resonant Regime --
+# M. Mayle, G. Quemener, B. P. Ruzic, and J. L. Bohn, Phys. Rev. A 87, 012709
+# (2013).
+for system in MAYLE_ALKALIDIMERPLUSDIMER.systems.keys():
+    dos, lt = MAYLE_ALKALIDIMERPLUSDIMER.get_dos_MoleculeMolecule(system,
+                                                                  J=J,
+                                                                  MQN=MQN,
+                                                                  nmax=NMAX,
+                                                                  vmax=VMAX)
+    print system, dos, lt
 
-for system in Jfec_AlkaliDimerPlusDimer.systems.keys():
- dos, lt = Jfec_AlkaliDimerPlusDimer.get_dos_MoleculeMolecule(system,J=J,MQN=MQN,nmax=nmax,vmax=vmax)
- print system, dos, lt
-
+for system in JFEC_ALKALIDIMERPLUSDIMER.systems.keys():
+    dos, lt = JFEC_ALKALIDIMERPLUSDIMER.get_dos_MoleculeMolecule(system,
+                                                                 J=J,
+                                                                 MQN=MQN,
+                                                                 nmax=NMAX,
+                                                                 vmax=VMAX)
+    print system, dos, lt
