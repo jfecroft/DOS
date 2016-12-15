85c7b22c57c74b1cb6bb1fb3fb74508cabda0a50
jfecroft <j.f.e.croft@gmail.com>
2016-12-15 13:54:01 -0800

    improve usabilty
    not sure why i thought using a class was a good idea

--- DOSModule.py
+++ DOSModule.py
@@ -51,6 +51,73 @@ def _read_data(dir_loc, inputfile, qnmax):
     return bound_states, qnmax
 
 
+def compute_dos(jqn, mqn, bound_states_c, bound_states_d, vmax,
+         energy_range=10.0, energy_offset=0.0):
+    """
+    computes the dos for an atom moleucle collsiions as described in
+    Statistical aspects of ultracold resonant scattering Mayle, Ruzic and
+    Bohn
+    Variables
+    jqn       - total jqn quantum number.
+    mqn     - mj_qn projection quantum number.
+    bound_states_c - boundstates of the complex obtained from _read_data
+    bound_states_d - boundstates of the molecule obtained from _read_data
+    vmax    - maximum vibrational quantum number.
+    energy_range  - dos = number of states counted/energy range
+    energy_offset - calculate dos around energy = energy_offset
+    """
+    if abs(mqn) > abs(jqn):
+        raise Exception('physically impossible (abs(mj_qn) > abs(jqn))')
+    limit = (energy_range/2.0)*K2EH  # convert energy_range K to hartrees
+    energy_offset *= K2EH
+    outside = np.ma.masked_outside  # mask enties in list outside range
+    count = np.ma.count  # counts the number of unmasked entries in a list
+    abs_gs = bound_states_d[0][0]
+    num = 0
+    # looping over rotational state of dimer a
+    for nqn in xrange(len(bound_states_d)):
+        # looping over all l constant with jqn
+        for lqn in xrange(len(bound_states_c)):
+            # only include pairs which couple to form jqn
+            if abs(nqn-lqn) <= jqn and nqn+lqn >= jqn:
+                # looping over all vibrational levels of a
+                for vqn in xrange(min(len(bound_states_d[nqn]), vmax+1)):
+                    # degeneracy
+                    deg = len(xrange(max(-lqn, mqn-nqn),
+                                     min(lqn, mqn+nqn)))+1
+                    threshold_energy = bound_states_d[nqn][vqn]-abs_gs
+                    num += count(
+                        outside(bound_states_c[lqn],
+                                -limit-threshold_energy+energy_offset,
+                                limit-threshold_energy+energy_offset))*deg
+
+    dos = (float(num)/energy_range)*1.0E-3
+    lifetime = dos*1.0E3*EH2K*2.0*pi*ATOMICUNITOFTIME*1.0e9
+    # return dos in mK-1 and lifetime in ns
+    return dos, lifetime
+
+
+def get_dos(cmplx_dirn, cmplx_filen, dimer_dirn, dimer_filen,
+            jqn=0, mqn=0, nmax=100, vmax=9999,
+            energy_offset=0.0, **kwargs):
+    """
+    simple wrapper around _read_data and AMDOS
+    Variables
+    jqn       - total J quantum number.
+    mqn     - mj_qn projection quantum number.
+    nmax    - maximum allowed rotaional quantum number of the molecule.
+    vmax    - maximum allowed vibrational quantum number of the molecule.
+    energy_offset - calculate dos around energy = energy_offset
+    """
+    lmax = nmax + jqn
+    bound_states_d, nmax = _read_data(dimer_dirn, dimer_filen, nmax)
+    bound_states_c, lmax = _read_data(cmplx_dirn, cmplx_filen, lmax)
+    dos, lifetime = compute_dos(jqn, mqn, bound_states_c, bound_states_d,
+                                vmax, energy_offset=energy_offset)
+    return dos, lifetime
+
+
+
 class AMDOS(object):
     """
     Atom molecule density of states class
@@ -147,34 +214,6 @@ class AMDOS(object):
                             num_open += 1
         return num_open
 
-    def get_dos(self, key, jqn=0, mqn=0, nmax=100, vmax=9999,
-                energy_offset=0.0):
-        """
-        simple wrapper around _read_data and AMDOS
-        Variables
-        jqn       - total J quantum number.
-        mqn     - mj_qn projection quantum number.
-        nmax    - maximum allowed rotaional quantum number of the molecule.
-        vmax    - maximum allowed vibrational quantum number of the molecule.
-        energy_offset - calculate dos around energy = energy_offset
-        """
-        lmax = nmax + jqn
-        self.bound_states_d, self.nmax = _read_data(
-            self.systems[key]['dimer_dirn'],
-            self.systems[key]['dimer_filen'],
-            nmax)
-        self.bound_states_c, self.lmax = _read_data(
-            self.systems[key]['cmplx_dirn'],
-            self.systems[key]['cmplx_filen'],
-            lmax)
-        self.dos, self.lifetime = AMDOS._dos(
-            jqn,
-            mqn,
-            self.bound_states_c,
-            self.bound_states_d,
-            vmax,
-            energy_offset=energy_offset)
-        return self.dos, self.lifetime
 
     def get_num_open(self, key, jqn=0, mqn=0, nmax=100, vmax=9999,
                      energy_offset=0.0):
--- dos_atom_molecule.py
+++ dos_atom_molecule.py
@@ -1,54 +1,43 @@
 """
 Reproduce published results
 """
-import DOSModule
+from DOSModule import get_data, get_dos
+from tabulate import tabulate
 
 FILEN = 'Jfec_AlkaliAtomPlusDimer.yml'
-JFEC_ALKALIATOMPLUSDIMER = DOSModule.AMDOS(FILEN)
+JFEC_ALKALIATOMPLUSDIMER = get_data(FILEN)
 FILEN = 'Mayle_AlkaliAtomPlusDimer.yml'
-MAYLE_ALKALIATOMPLUSDIMER = DOSModule.AMDOS(FILEN)
+MAYLE_ALKALIATOMPLUSDIMER = get_data(FILEN)
 FILEN = 'HeliumPlusHydrocarbon.yml'
-HELIUMPLUSHYDROCARBON = DOSModule.AMDOS(FILEN)
+HELIUMPLUSHYDROCARBON = get_data(FILEN)
 
 
-JQN, MQN = 0, 0  # total J quantum number
-NMAX, VMAX = 100, 9999  # maximum rotational and vibrational quantum numbers
+# JQN, MQN = 0, 0  # total J quantum number
+# NMAX, VMAX = 100, 9999  # maximum rotational and vibrational quantum numbers
 # lmax is determined by JQN and NMAX
-COLLISION_ENERGY = 0.0
+# COLLISION_ENERGY = 0.0
 
-print 'system  dos(mK-1) lt(ns)'
+header = ['system', 'dos(mK-1)','lt(ns)']
+output = []
 # printing dos and lt for alkali atom + dimer collisions as published in
 # Long-Lived Complexes and Chaos in Ultracold Molecular Collisions
 #            J. F. E. Croft and J. L. Bohn, Phys. Rev. A 89, 102714 (2014).
-for system in JFEC_ALKALIATOMPLUSDIMER.systems.keys():
-    dos, lt = JFEC_ALKALIATOMPLUSDIMER.get_dos(system,
-                                               jqn=JQN,
-                                               mqn=MQN,
-                                               nmax=NMAX,
-                                               vmax=VMAX,
-                                               energy_offset=COLLISION_ENERGY)
-    print system, dos, lt
+for system, input_dict in JFEC_ALKALIATOMPLUSDIMER.iteritems():
+    dos, lt = get_dos(**input_dict)
+    output.append([system, dos, lt])
 
 
 # printing dos and lt for alkali atom + dimer collisions as published in
 # statistical Aspects of Ultracold Resonant Scattering --
 # M. Mayle, B. P. Ruzic, and J. L. Bohn, Phys. Rev. A 85, 062712 (2012).
-for system in MAYLE_ALKALIATOMPLUSDIMER.systems.keys():
-    dos, lt = MAYLE_ALKALIATOMPLUSDIMER.get_dos(system,
-                                                jqn=JQN,
-                                                mqn=MQN,
-                                                nmax=NMAX,
-                                                vmax=VMAX,
-                                                energy_offset=COLLISION_ENERGY)
-    print system, dos, lt
+for system, input_dict in MAYLE_ALKALIATOMPLUSDIMER.iteritems():
+    dos, lt = get_dos(**input_dict)
+    output.append([system, dos, lt])
 
 # printing dos and lt for helium + hydrocarbon collisions
 # as yet unpublished
-for system in HELIUMPLUSHYDROCARBON.systems.keys():
-    dos, lt = HELIUMPLUSHYDROCARBON.get_dos(system,
-                                            jqn=JQN,
-                                            mqn=MQN,
-                                            nmax=NMAX,
-                                            vmax=VMAX,
-                                            energy_offset=COLLISION_ENERGY)
-    print system, dos, lt
+for system, input_dict in HELIUMPLUSHYDROCARBON.iteritems():
+    dos, lt = get_dos(**input_dict)
+    output.append([system, dos, lt])
+
+print tabulate(output, headers=header)
